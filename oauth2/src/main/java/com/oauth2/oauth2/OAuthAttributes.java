0603cedfa0a64d11cddd1b627ea75286366d58c4
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2021-01-31 02:54:19 +0900

    Naver OAuth2 설정 추가

--- oauth2/src/main/java/com/oauth2/oauth2/OAuthAttributes.java
+++ oauth2/src/main/java/com/oauth2/oauth2/OAuthAttributes.java
@@ -9,10 +9,12 @@ import java.util.Map;
 
 @Getter
 public class OAuthAttributes {
+
     private Map<String, Object> attributes;
     private String nameAttributeKey, name, email, picture;
+
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,
+    public OAuthAttributes(Map<String, Object> attributes,              //OAuth2User 의 사용자 정보 반환값은 Map 이라서 하나하나 변환
                            String nameAttributeKey,
                            String name, String email, String picture) {
         this.attributes = attributes;
@@ -21,11 +23,20 @@ public class OAuthAttributes {
         this.email = email;
         this.picture = picture;
     }
+
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        //Naver 일 때
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+
+    //Google
     public static OAuthAttributes ofGoogle(String userNameAttributeName,
                                            Map<String, Object> attributes) {
         return OAuthAttributes.builder()
@@ -36,12 +47,27 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    //Naver
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    //Member Entity 생성 -> 처음 가입 할 때
     public Member toEntity() {
         return Member.builder()
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.USER)
+                .role(Role.USER)    //기본 권한 USER
                 .build();
     }
 }
